2715b824b87978867e97bffe57674f592af50a31
Lokendra Dangi <pachar.com@gmail.com>
2020-08-31 21:39:30 +0545

    Fetch id of post from firebase

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ function App() {
 
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
-      setPosts(snapshot.docs.map((doc) => doc.data()));
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
     });
   }, []);
 
@@ -22,8 +27,9 @@ function App() {
           alt="instagram logo"
         />
       </div>
-      {posts.map((post) => (
+      {posts.map(({ id, post }) => (
         <Post
+          key={id}
           username={post.username}
           caption={post.caption}
           imageUrl={post.imageUrl}
